daeffca5a34d9dc84fcafd07e5e6ca4f9371b8f1
Jacek Mosiński <103215190+Scorpion19702022@users.noreply.github.com>
2023-04-02 01:20:49 +0200

    Add files via upload
    start page live

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const burgerBtn = document.querySelector('.burger')
+const btnBars = document.querySelector('.fa-bars')
+const btnCross = document.querySelector('.fa-xmark')
+const nav = document.querySelector('.navbar')
+const links = document.querySelectorAll('.navbar__link')
+
+// --------------------------------------
+
+const cardHomeOne = document.querySelector('.cards__cardone')
+const cardhomeTwo = document.querySelector('.cards__cardtwo')
+const body = document.querySelector('body')
+const imgCardOne = document.querySelector('.imgcardone')
+const imgCardTwo = document.querySelector('.imgcardtwo')
+
+body.onload = function () {
+	loadCards()
+}
+
+const loadCards = () => {
+	setTimeout(() => {
+		cardHomeOne.classList.add('actioncards')
+	}, 800)
+	setTimeout(() => {
+		cardhomeTwo.classList.add('actioncards')
+	}, 1600)
+	setTimeout(() => {
+		imgCardOne.classList.add('imgcardoneview')
+	}, 2400)
+	setTimeout(() => {
+		imgCardTwo.classList.add('imgcardoneview')
+	}, 3200)
+}
+
+// ------------------------------------------
+
+const handleNav = () => {
+	nav.classList.toggle('actionnav')
+	btnBars.classList.toggle('hide')
+	btnCross.classList.toggle('hide')
+
+	links.forEach(items => {
+		items.addEventListener('click', () => {
+			nav.classList.remove('actionnav')
+			btnBars.classList.remove('hide')
+			btnCross.classList.add('hide')
+		})
+	})
+}
+
+burgerBtn.addEventListener('click', handleNav)
